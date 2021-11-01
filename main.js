650026bcb87bdfea2cd906728b9f85ade34d3d97
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-11-01 18:40:32 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+var song = "";
+
+var left_y = 0;
+var right_y = 0;
+
+var left_x = 0;
+var right_x = 0; 
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        
+        right_x = results[0].pose.rightWrist.x;
+        right_y = results[0].pose.rightWrist.y;
+        console.log("The y position of the right wrist is " + right_y);
+
+        left_x = results[0].pose.leftWrist.x;
+        left_y = results[0].pose.leftWrist.y;
+        console.log("The y position of the left wrist is " + left_y);
+    }
+}
+
+function modelLoaded() {
+    console.log("❁poseNet modal initialized❁")
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000"); 
+    circle(left_x, left_y, 20);
+    circle(right_x, right_y, 20);
+
+
+    if(right_y > left_y) {
+        play();
+    } else {
+        pause()
+    }
+}
+
+function play() {
+    song.play();
+}
+
+function pause() {
+    song.pause();
+}
+
